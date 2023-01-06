73295b87945f27b1e50dc824274f77b29ffe4f20
Louis Marzorati <LouisMarzorati@users.noreply.github.com>
2023-01-06 14:04:40 -0500

    copy change

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -2,7 +2,7 @@ export default function LandingPage({}) {
   return (
     <div className='flex flex-col items-center justify-center'>
       <h1 className='text-4xl font-bold'>Welcome to GRBP</h1>
-      <p className='text-xl'>Sign in to continue the cuckle</p>
+      <p className='text-xl'>Sign in to continue</p>
     </div>
   )
 }
